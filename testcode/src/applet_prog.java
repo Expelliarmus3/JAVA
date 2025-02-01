263c0e7c20f388d6d04ba198bd7e1d6932dc19a2
Ahana Sarkar <ahanasarkar66@gmail.com>
2025-02-01 23:14:20 +0530

    Applet program note that applet is deprecated code does not work

---
+++ testcode/src/applet_prog.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.awt.Graphics;
+import java.applet.*;
+
+public class applet_prog extends Applet{
+    public void paint(Graphics g) {
+        g.drawString("Hello World",150,150);
+    }
+}
